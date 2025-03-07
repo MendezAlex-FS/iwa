7b4c86f9ff35a3f239e7288a4f549efdcc9356b8
MendezAlex-FS <AMendez4@student.fullsail.edu>
2025-03-07 10:09:39 -0500

    Made changes to file names and code

--- ITD1/Mendez_Alex_CE01/JS/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Alert shows the correct phrase and works correctly.
-  alert("My JavaScript file is connected properly!");
-
-  // Console shows the correct phrase and works correctly.
-  console.log("I can display text in the Console!");
-});
---
+++ ITD1/Mendez_Alex_CE01/JS/script.js
@@ -0,0 +1,5 @@
+// Alert shows the correct phrase and works correctly.
+alert("My JavaScript file is connected properly!");
+
+// Console shows the correct phrase and works correctly.
+console.log("I can display text in the Console!");
