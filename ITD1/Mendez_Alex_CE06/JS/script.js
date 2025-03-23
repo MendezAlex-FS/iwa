040006c4beb4b345c053a48e5c0470d0d9d46b0b
MendezAlex-FS <AMendez4@student.fullsail.edu>
2025-03-23 19:41:23 -0400

    Changes for CE06

--- ITD1/Mendez_Alex_CE06/JS/script.js
+++ ITD1/Mendez_Alex_CE06/JS/script.js
@@ -1,110 +1,72 @@
 /*
 Alex Mendez
 3/23/2025
-CE05 QuerySelector
+CE05 Html Forms
 */
 
-// Say what we are doing.
-alert(
-  "Hello and welcome to the Discount Double check." +
-    "You will be asked to provide the prices of two items, " +
-    "which will be used to calculate the total, including any applicable discounts."
-);
+// Add event listener for the button.
+document.querySelector("#convertButton").addEventListener("click", function (e) {
+    if (document.querySelector("#gallons").reportValidity()) {
+        // Get the input values
+        let gallonsValue = parseFloat(document.querySelector("#gallons").value);
+        let measurment = document.querySelector("[name='measurements']:checked").value;
 
-// You will be prompting the user for the price of 2 items bought at a store
-let cost1 = parseFloat(prompt("Cost of First Item (in $)"));
-let cost2 = parseFloat(prompt("Cost of Second Item (in $)"));
+        // Get the result elements
+        let quartsResult = document.querySelector("#quartsResult");
+        let pintsResult = document.querySelector("#pintsResult");
+        let cupsResult = document.querySelector("#cupsResult");
 
-let total = cost1 + cost2;
-let discount = 0;
-let discountDisplay = "";
-let resultsClass = "";
-let message = "";
+        // Change all boxes to 0
+        quartsResult.value = "0";
+        pintsResult.value = "0";
+        cupsResult.value = "0";
 
-// Figure out the discount
-if (total >= 100) {
-  discount = 0.1;
-  discountDisplay = "10%";
-  resultsClass = "green-result";
-} else if (total >= 50) {
-  discount = 0.05;
-  discountDisplay = "5%";
-  resultsClass = "green-result";
-} else {
-  discountDisplay = "no";
-  resultsClass = "yellow-result";
-}
+        // Do the convertions and populate results
+        if (measurment === "quartz") {
+            quartsResult.value = gallonsValue * 4;
+        } else if (measurment === "pints") {
+            pintsResult.value = gallonsValue * 8;
+        } else {
+            cupsResult.value = gallonsValue * 16;
+        }
+    }
+})
 
-// Give them a discount based on their total
-if (discount > 0) {
-  let finalTotal = total - total * discount;
-  message =
-    "Your total purchase is " +
-    finalTotal.toFixed(2) +
-    ", which includes your " +
-    discountDisplay +
-    " discount.";
-} else {
-  message = "Your total purchase is $" + total.toFixed(2) + ".";
-}
+// Add event listener for the rank result button button.
+document.querySelector("#rankingButton").addEventListener("click", function (e) {
+    // Validate the model
+    if (document.querySelector("#dvds").reportValidity() &&
+        document.querySelector("#bluRays").reportValidity() &&
+        document.querySelector("#ultraViolets").reportValidity()) {
 
-// Show the result in an element using querySelector
-const results = document.querySelector("#results");
-results.innerHTML = message;
-results.classList.add(resultsClass);
+        // Get the values from the text boxes and initiate variables
+        let dvds = parseInt(document.querySelector("#dvds").value);
+        let bluRays = parseInt(document.querySelector("#bluRays").value);
+        let ultraViolets = parseInt(document.querySelector("#ultraViolets").value);
+        let message = "";
+        let imageSource = "IMG/bronzeStar.jpg";
 
-// Rest reusables
-message = "";
-resultsClass = "";
+        // Add all the movies and prepare messages and image sources.
+        let movies = dvds + bluRays + ultraViolets;
+        if (movies < 50) {
+            message = `Your collection contains only ${movies} movies, which makes you a bronze star member.`;
+            imageSource = "IMG/bronzeStar.jpg";
+        } else if (movies >= 50 && movies < 100) {
+            message = `Your collection contains ${movies} movies, which makes you a silver star member.`;
+            imageSource = "IMG/silverStar.jpg";
+        } else if (movies >= 100) {
+            message = `Wow! Your collection contains ${movies} movies, which makes you a gold star member.`;
+            imageSource = "IMG/goldStar.jpg";
+        }
 
-// Let them know we are checking on their gas.
-alert("Now, let's see if you can cross the desert with the gas you have!");
+        // Set the message and make it visible
+        const collectionResults = document.querySelector("#collectionResults");
+        collectionResults.innerHTML = message
+        collectionResults.removeAttribute("class");
 
-let maxGallons = parseInt(
-  prompt("What is the maximum number of gallons your car tank holds?")
-);
-let gasPercent = parseInt(prompt("How full is your gas tank? (in %)"));
-let milePerGalon = parseInt(
-  prompt("How many miles per gallon does your car get?")
-);
-
-const gallonsLeft = maxGallons * (gasPercent / 100);
-const milesCanGo = gallonsLeft * milePerGalon;
-let imageSource = "";
-let imageAltText = "Not enough gas";
-
-if (milesCanGo >= 200) {
-  resultsClass = "green-result";
-  imageSource = "IMG/enoughGas.jpg";
-  imageAltText = "You have enough gas image";
-  message =
-    "Yes, you can drive " +
-    milesCanGo +
-    " miles and you can make it without stopping for gas!";
-} else {
-  resultsClass = "yellow-result";
-  imageSource = "IMG/notEnoughGas.jpg";
-  imageAltText = "Not enough gas image";
-  message =
-    "You can only drive " +
-    milesCanGo +
-    " miles more, better get gas now while you can!";
-}
-
-// Add events to buttons
-document
-  .querySelector("#showTextAnswer")
-  .addEventListener("click", function (e) {
-    const textResults = document.querySelector("#textResults");
-    textResults.innerHTML = message;
-    textResults.classList = "";
-    textResults.classList.add(resultsClass);
-  });
-document
-  .querySelector("#showGraphicsAnswer")
-  .addEventListener("click", function (e) {
-    const imageResults = document.querySelector("#imageResults");
-    imageResults.src = imageSource;
-    imageResults.alt = imageAltText;
-    imageResults.classList = "";
-  });
+        // Set the image source and make it visible
+        const rankLevelImage = document.querySelector("#rankLevelImage");
+        rankLevelImage.src = imageSource;
+        rankLevelImage.removeAttribute("class");
+    }
+})
