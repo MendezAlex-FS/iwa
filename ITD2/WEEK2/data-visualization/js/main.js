24ce11731ab42e4dd9825d4d88d7357c26d22d9b
MendezAlex-FS <AMendez4@student.fullsail.edu>
2025-05-17 10:48:40 -0400

    Final changes for data visualization

--- ITD2/WEEK2/data-visualization/js/main.js
+++ ITD2/WEEK2/data-visualization/js/main.js
@@ -1,17 +1,18 @@
 "use strict";
 
 (function () {
-    const form = document.querySelector("#viewsForm");
+    const button = document.querySelector("button");
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
+    const averageViews = document.querySelector("p");
 
-    form.addEventListener("submit", createChart);
+    button.addEventListener("click", createChart);
 
     function createChart(e) {
         // Stop the button from posting
         e.preventDefault();
 
-        // Validate the form 
+        // Validate the form fields
         if (document.querySelector("#minViews").reportValidity() &&
             document.querySelector("#maxViews").reportValidity()) {
 
@@ -30,33 +31,20 @@
 
             // Declare and initialize the constants
             const spacing = 25;
-            const barWidth = 60;
+            const width = 60;
             const offsetX = 50;
             const offsetY = 350;
 
+            // This one is changed in the loop so it cannot be a const.
             let total = 0;
 
             for (let i = 0; i < 5; i++) {
                 // Generate the bar number for the bar
-                const barNumber = Math.floor(Math.random() * (maxViews - minViews + 1)) + minViews;
-                total += barNumber;
+                const randomNumber = Math.floor(Math.random() *
+                    (maxViews - minViews + 1)) + minViews;
+                total += randomNumber;
 
-                // Set the bar height and (x, y) coordinates
-                const barHeight = barNumber / maxViews * 300;
-                const x = offsetX + i * (barWidth + spacing);
-                const y = offsetY - barHeight;
-
-                // Display the views above the bar centered
-                ctx.fillStyle = "#000";
-                ctx.font = "14px Arial";
-                ctx.textAlign = "center";
-                ctx.fillText(barNumber, x + barWidth / 2, y - 10);
-
-                // Display the bar
-                ctx.fillStyle = "darkblue";
-                ctx.fillRect(x, y, barWidth, barHeight);
-
-                // We can't use arrays, so get the label for the itteration.
+                // We can't use arrays, so get the bottom label for bar created.
                 let dayLabel = "";
                 if (i === 0) dayLabel = "Mon";
                 else if (i === 1) dayLabel = "Tue";
@@ -64,14 +52,30 @@
                 else if (i === 3) dayLabel = "Thu";
                 else if (i === 4) dayLabel = "Fri";
 
-                // Display the day label
-                ctx.fillText(dayLabel, x + barWidth / 2, offsetY + 20);
+                // Set the bar height and (x, y) coordinates
+                const height = randomNumber / maxViews * 300;
+                const x = offsetX + i * (width + spacing);
+                const y = offsetY - height;
 
-                // Display the average
+                // Display the day label at the bottom leaving space
+                // for the average
+                ctx.fillText(dayLabel, x + width / 2, offsetY + 20);
+
+                // Display the bar above the day label
+                ctx.fillStyle = "darkblue";
+                ctx.fillRect(x, y, width, height);
+
+                // Display the views above the bar centered
+                ctx.fillStyle = "#000";
+                ctx.font = "14px Arial";
+                ctx.textAlign = "center";
+                ctx.fillText(randomNumber, x + width / 2, y - 10);
+
+                // Display the average at the bottom. To be shown somewhere in the HTML.
                 if (i === 4) {
                     const average = Math.round(total / 5);
                     const averageText = `Average Views: ${average}`;
-                    ctx.fillText(averageText, canvas.width / 2, offsetY + 60);
+                    averageViews.innerHTML = averageText;
                 }
             }
         }
