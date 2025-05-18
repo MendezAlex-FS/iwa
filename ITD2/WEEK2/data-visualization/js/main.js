921a94e40a5ef6fe381cad5fdb67572f03eb19e4
MendezAlex-FS <AMendez4@student.fullsail.edu>
2025-05-18 09:38:10 -0400

    Fixed font issue with first day.

--- ITD2/WEEK2/data-visualization/js/main.js
+++ ITD2/WEEK2/data-visualization/js/main.js
@@ -23,6 +23,13 @@
             const minViews = parseInt(document.querySelector("#minViews").value);
             const maxViews = parseInt(document.querySelector("#maxViews").value);
 
+            // Declare and initialize the variables and contants
+            const spacing = 10;
+            const width = 50;
+            const offsetX = 50;
+            const offsetY = 350;
+            let total = 0;
+
             // Make sure minViews is less or equal to maxViews
             if (minViews > maxViews) {
                 alert("Minimum must be less than or equal to maximum.");
@@ -32,51 +39,45 @@
             // Clear the canvas before using it.
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            // Declare and initialize the constants
-            const spacing = 25;
-            const width = 60;
-            const offsetX = 50;
-            const offsetY = 350;
-
-            // This one is changed in the loop so it cannot be a const.
-            let total = 0;
-
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 7; i++) {
                 // Generate the bar number for the bar
                 const randomNumber = Math.floor(Math.random() *
                     (maxViews - minViews + 1)) + minViews;
                 total += randomNumber;
 
-                // We can't use arrays, so get the bottom label for bar created.
+                // We can't use arrays, so get the bottom label for the bar created.
                 let dayLabel = "";
-                if (i === 0) dayLabel = "Mon";
-                else if (i === 1) dayLabel = "Tue";
-                else if (i === 2) dayLabel = "Wed";
-                else if (i === 3) dayLabel = "Thu";
-                else if (i === 4) dayLabel = "Fri";
+                if (i === 0) dayLabel = "Sun";
+                else if (i === 1) dayLabel = "Mon";
+                else if (i === 2) dayLabel = "Tue";
+                else if (i === 3) dayLabel = "Wed";
+                else if (i === 4) dayLabel = "Thu";
+                else if (i === 5) dayLabel = "Fri";
+                else if (i === 6) dayLabel = "Sat";
 
                 // Set the bar height and (x, y) coordinates
                 const height = randomNumber / maxViews * 300;
                 const x = offsetX + i * (width + spacing);
                 const y = offsetY - height;
 
-                // Display the day label at the bottom leaving space
-                // for the average
+                // Set the font styles
+                ctx.fillStyle = "#000";
+                ctx.font = "14px Arial";
+                ctx.textAlign = "center";
+
+                // Display the views above the bar centered
+                ctx.fillText(randomNumber, x + width / 2, y - 10);
+
+                // Display the day label at the bottom leaving space for the average
                 ctx.fillText(dayLabel, x + width / 2, offsetY + 20);
 
                 // Display the bar above the day label
                 ctx.fillStyle = "darkblue";
                 ctx.fillRect(x, y, width, height);
 
-                // Display the views above the bar centered
-                ctx.fillStyle = "#000";
-                ctx.font = "14px Arial";
-                ctx.textAlign = "center";
-                ctx.fillText(randomNumber, x + width / 2, y - 10);
-
                 // Display the average at the bottom. To be shown somewhere in the HTML.
                 if (i === 4) {
-                    const average = Math.round(total / 5);
+                    const average = Math.round(total / 7);
                     const averageText = `Average Views: ${average}`;
                     averageViews.innerHTML = averageText;
                 }
